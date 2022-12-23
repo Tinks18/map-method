d3361ab9045e137633a86fd7a29e965bbed63aa1
Tinks18 <tinutani9@gmail.com>
2022-12-23 13:10:35 +0000

    Final commit

--- map.js
+++ map.js
@@ -5,16 +5,29 @@
  */
 
 // Using a for loop
+let nums = [1,2,3,4,5];
+let results = [];
+for (let num of nums){
+  results.push(num*2)
+}
+console.log(results);
 
 
 // Using map()
-
+const multByTwo =function (num){
+  return num * 2;
+}
+const mapResults = nums.map(multByTwo);
+console.log(mapResults);
 
 // Simplified w/ map()
-
+//const multByTwo2 = nums.map =>(function(num) return num * 2);
+const simplified = nums.map(function (num){return num * 2 });
+console.log(simplified);
 
 // Simplfied w/ map() + arrow function
-
+const arrow = nums.map(num => num * 2);
+console.log(arrow);
 
 // With objects:
 const students = [
@@ -37,3 +50,16 @@ const students = [
     skill: 'CSS'
   },
 ];
+
+// const arrow = nums.map(num => num * 2);
+// console.log(arrow);
+const studentsWithIds2 = students.map(student => (
+  {
+    id: student.id,
+    name: student.name
+  } 
+));
+ const studentsWithIds = students.map(student => [student.id , student.name] ) ;
+
+ console.log(studentsWithIds);
+console.log(studentsWithIds2);
